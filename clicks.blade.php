63fcd4de766df7af6179db14e0e85e2491fa16ed
natann007 <54375371+natann007@users.noreply.github.com>
2020-11-03 20:49:58 +0200

    Add files via upload

---
+++ clicks.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>User Clicks</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 42px;
+            }
+
+            .links > a {
+                padding: 26px 0;
+                font-size: 18px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .table caption{
+               font-size: 18px;
+               font-weight: bold;
+               caption-side: top!important;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    User Clicks
+                </div>
+                <div class="links text-primary">
+                    <a href="{{url('/')}}">Back to Homepage</a>
+                </div>
+                <div id="clicks" class="mt-5" data="{{ $data }}"></div>
+            </div>
+        </div>
+        <script src="js/app.js"></script>
+    </body>
+</html>
